c9e2c256c35f3ea066c961cc5bd4c1806ebc7580
Drake McCabe <drakemccabe@gmail.com>
2015-09-29 19:49:54 -0400

    add events/articles controller model tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
-describe ArticlesController do
+describe ArticlesController, type: :controller do
+  describe "GET #index" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+  end
 
+  describe "GET #show" do
+      it "returns http success" do
+        article = FactoryGirl.create(:article)
+        get :show, article: article.id
+        expect(response).to have_http_status(:success)
+      end
+  end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
-describe EventsController do
+describe EventsController, type: :controller do
+  describe "GET #index" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+  end
 
+  describe "GET #show" do
+      it "returns http success" do
+        event = FactoryGirl.create(:event)
+        get :show, event: event.id
+        expect(response).to have_http_status(:success)
+      end
+  end
 end
