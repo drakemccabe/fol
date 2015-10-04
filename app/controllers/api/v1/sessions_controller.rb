250e67f301d7f903a1613f4b90135645a8dfe790
Drake McCabe <drakemccabe@gmail.com>
2015-10-04 08:22:48 -0400

    update sessions controller for possibility of nil

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,13 +5,15 @@ class Api::V1::SessionsController < ApplicationController
     user_email = params[:session][:email]
     user = user_email.present? && User.find_by(email: user_email)
 
-    if user.valid_password? user_password
-      sign_in user, store: false
-      user.generate_authentication_token!
-      user.save
-      render json: user, status: 200, location: [:api, user]
-    else
-      render json: { errors: "Invalid email or password" }, status: 422
+    unless user.nil?
+      if user.valid_password? user_password
+        sign_in user, store: false
+        user.generate_authentication_token!
+        user.save
+        render json: user, status: 200, location: [:api, user]
+      else
+        render json: { errors: "Invalid email or password" }, status: 422
+      end
     end
   end
 
