d1853d3bf331f4d61b0890a324fd2ffaccb7c592
Drake McCabe <drakemccabe@gmail.com>
2015-10-05 18:04:50 -0400

    contacts controller add show method

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::ContactsController < ApplicationController
+  respond_to :json
+
+  def show
+    respond_with Contact.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
      resources :users, only: [:show, :create, :update, :destroy]
      resources :sessions, only: [:create, :destroy]
      resources :donations, only: [:show, :index, :create]
+     resources :contacts, only: [:show]
    end
  end
 end
