a1269c860311e6aa48ff6bd7d7dbcad65eccb4c0
Drake McCabe <drakemccabe@gmail.com>
2015-10-05 18:21:18 -0400

    add contacts controller create method test

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::ContactsController < ApplicationController
   def show
     respond_with Contact.find(params[:id])
   end
+
+  def index
+    respond_with Contact.all
+  end
 end
--- spec/controllers/api/v1/contacts_controller_spec.rb
+++ spec/controllers/api/v1/contacts_controller_spec.rb
@@ -4,6 +4,7 @@ describe Api::V1::ContactsController do
   describe "GET #show" do
     before(:each) do
       @contact = FactoryGirl.create :contact
+      api_authorization_header user.auth_token
       get :show, id: @contact.id
     end
 
@@ -18,6 +19,7 @@ describe Api::V1::ContactsController do
   describe "GET #index" do
     before(:each) do
       4.times { FactoryGirl.create :contact }
+      api_authorization_header user.auth_token
       get :index
     end
 
@@ -27,5 +29,42 @@ describe Api::V1::ContactsController do
     end
 
     it { should respond_with 200 }
+  end
+
+  describe "POST #create" do
+    context "when contact is successfully made" do
+      before(:each) do
+        @contact_attributes = FactoryGirl.attributes_for :contact
+        api_authorization_header user.auth_token
+        post :create, { contact: @contact_attributes }
+      end
+
+      it "renders json for the contact just created" do
+        contact_response = json_response
+        expect(contact_response[:first_name]).to eql @contact_attributes[:first_name]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when fails to save" do
+      before(:each) do
+        @invalid_contact_attributes = { state: "Massachusetts" }
+        api_authorization_header user.auth_token
+        post :create, { contact: @invalid_contact_attributes }
+      end
+
+      it "renders an errors json" do
+        contact_response = json_response
+        expect(contact_response).to have_key(:errors)
+      end
+
+      it "returns error messages" do
+        contact_response = json_response
+        expect(contact_response[:errors][:state]).to include "is not a number"
+      end
+
+      it { should respond_with 422 }
+    end
   end
 end
