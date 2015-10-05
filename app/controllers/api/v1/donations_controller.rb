d5d82841ecd09c553132a7f21fe684996d8bac3f
Drake McCabe <drakemccabe@gmail.com>
2015-10-05 11:37:51 -0400

    add create method to events controller

--- app/controllers/api/v1/donations_controller.rb
+++ app/controllers/api/v1/donations_controller.rb
@@ -8,4 +8,20 @@ class Api::V1::DonationsController < ApplicationController
   def index
     respond_with Donation.all.order(:created_at).limit(1000)
   end
+
+  def create
+    #add donation class
+    donation = donation.new(donation_params)
+    if donation.save
+      render json: product, status: 201, location: [:api, product]
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:amount, :contact_id)
+  end
 end
