42ec506a30017548227daec9a549669027ea91de
Drake McCabe <drakemccabe@gmail.com>
2015-10-05 20:16:49 -0400

    add index and show methods, tests

--- app/controllers/api/v1/atricles_controller.rb
+++ app/controllers/api/v1/atricles_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::AtriclesController < ApplicationController
+  before_action :authenticate_with_token!
+  respond_to :json
+
+  def show
+    respond_with Product.find(params[:id])
+  end
+
+  def index
+    respond_with Product.all
+  end
 end
--- spec/controllers/api/v1/atricles_controller_spec.rb
+++ spec/controllers/api/v1/atricles_controller_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe Api::V1::AtriclesController do
   describe "GET #show" do
       before(:each) do
+        user = FactoryGirl.create :user
+        api_authorization_header user.auth_token
         @article = FactoryGirl.create :article
         get :show, id: @article.id
       end
@@ -14,5 +16,22 @@ describe Api::V1::AtriclesController do
 
       it { should respond_with 200 }
     end
+
+    describe "GET #index" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        api_authorization_header user.auth_token
+        4.times { FactoryGirl.create :article }
+        get :index
+      end
+
+      it "returns 4 records from the database" do
+        article_response = json_response
+        expect(article_response[:articles]).to have(4).items
+      end
+
+      it { should respond_with 200 }
+    end
+
 
 end
