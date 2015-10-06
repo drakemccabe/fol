b4779a7b503066c2ba65e233deaa03addf58dc42
Drake McCabe <drakemccabe@gmail.com>
2015-10-06 18:34:28 -0400

    add events controller INDEX

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::EventsController < ApplicationController
   def show
     respond_with Event.find(params[:id])
   end
+
+  def index
+    respond_with Event.all
+  end
 end
--- spec/controllers/api/v1/events_controller_spec.rb
+++ spec/controllers/api/v1/events_controller_spec.rb
@@ -2,16 +2,30 @@ require 'spec_helper'
 
 describe Api::V1::EventsController do
   describe "GET" do
-  before(:each) do
+    before(:each) do
        @event = FactoryGirl.create :event
        get :show, id: @event.id
-     end
+    end
 
-     it "returns the information about a single event" do
-       event_response = json_response
-       expect(event_response[:name]).to eql @event.name
-     end
+    it "returns the information about a single event" do
+      event_response = json_response
+      expect(event_response[:name]).to eql @event.name
+    end
 
-     it { should respond_with 200 }
-   end
+    it { should respond_with 200 }
+  end
+
+  describe "GET #index" do
+    before(:each) do
+      6.times { FactoryGirl.create :event }
+      get :index
+    end
+
+    it "returns 6 event objects in json" do
+      events_response = json_response
+      expect(events_response[:events]).to have(6).items
+    end
+
+    it { should respond_with 200 }
+  end
 end
