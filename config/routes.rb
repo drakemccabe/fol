6af5b4c2b1c3ce7c19a0c69c427f62eb0b137411
Drake McCabe <drakemccabe@gmail.com>
2015-10-05 20:11:24 -0400

    merge master

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
      resources :sessions, only: [:create, :destroy]
      resources :donations, only: [:show, :index, :create]
      resources :contacts, only: [:show, :index, :create, :update, :destroy]
+     resources :articles, only: [:show, :index, :create, :update, :destroy]
    end
  end
 end
