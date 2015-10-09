dbdaad8e7bcfdcf7383d08aaa64d0edbf9200d3a
Drake McCabe <drakemccabe@gmail.com>
2015-10-08 22:57:12 -0400

    add backend styling

--- app/assets/javascripts/components/start.jsx
+++ app/assets/javascripts/components/start.jsx
@@ -16,8 +16,8 @@ $.ajax({
 
       React.render(
       <Griddle results={rows} tableClassName="table" showFilter={true}
- showSettings={true} columns={["first_name", "last_name", "address", "city"]}
-resultsPerPage={5} enableInfiniteScroll={true} bodyHeight={400} useFixedHeader={true} onRowClick={editContact}
+ showSettings={true} columns={["id", "first_name", "last_name", "address", "city"]}
+resultsPerPage={5} enableInfiniteScroll={true} bodyHeight={600} useFixedHeader={true} onRowClick={editContact}
  />,
       document.getElementById('table')
       )
