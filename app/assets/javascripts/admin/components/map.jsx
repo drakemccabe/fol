0460e2bef3aa5c32739ee4786909a2721e777c87
Drake McCabe <drakemccabe@gmail.com>
2015-10-09 17:15:39 -0400

    added ajax call to map

--- app/assets/javascripts/admin/components/map.jsx
+++ app/assets/javascripts/admin/components/map.jsx
@@ -6,6 +6,25 @@ const coords = {
   lng: -71.007821
 };
 
+var markers = "";
+
+function loadMapData() {
+$.ajax({
+      url: "//api.fol.dev/contacts",
+      dataType: 'json',
+      cache: true,
+      success: function(data) {
+        markersFunction(data)
+      }
+    });
+  };
+
+  function markersFunction(data) {
+    markers = data["contacts"];
+    Run();
+  }
+
+
 const App = React.createClass({
 
   onMapCreated(map) {
@@ -25,6 +44,7 @@ const App = React.createClass({
   onClick(e) {
     console.log('onClick', e);
   },
+
 
   render() {
     return (
@@ -51,6 +71,11 @@ const App = React.createClass({
             lng={-71.016838}
             draggable={true}
             onDragEnd={this.onDragEnd} />
+
+            {markers.map(function(object, i){
+                return <Marker lat={markers[i]["latitude"]} lng={markers[i]["longitude"]} />;
+              })}
+
       </Gmaps>
     );
   }
@@ -68,5 +93,5 @@ function clearDiv() {
 $( "#link1" ).click(function() {
   event.preventDefault();
   clearDiv();
-  Run();
+  loadMapData();
 });
