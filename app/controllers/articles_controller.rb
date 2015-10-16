db2b3925af7944d7348a2b970ca3af74a6e1def1
Drake McCabe <drakemccabe@gmail.com>
2015-10-16 15:47:09 -0400

    update articles controller sort order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.order(:created_at).limit(4).reverse_order
-    @events = Event.order(:created_at).limit(3).reverse_order
+    @events = Event.where("event_date > ?", Date.today).order(:event_date).limit(3)
   end
 
   def show
